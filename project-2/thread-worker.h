1cd598520343058af29919740cd9040362bfbc77
Bryan Shangguan <shangguan.bryan@gmail.com>
2025-10-25 22:54:36 -0400

    Update thread-worker.h

--- project-2/thread-worker.h
+++ project-2/thread-worker.h
@@ -15,7 +15,7 @@
 #define _GNU_SOURCE
 
 /* To use Linux pthread Library in Benchmark, you have to comment the USE_WORKERS macro */
-// #define USE_WORKERS 1
+#define USE_WORKERS 1
 
 /* Targeted latency in milliseconds */
 #define TARGET_LATENCY   20  
